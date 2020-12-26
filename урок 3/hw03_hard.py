7089c0d984c28fc36d0e3bffa42e79cb79d02ac2
JeinL <lea1993@yandex.ru>
2020-12-26 16:15:10 +0300

    задача хард урок 3

--- урок 3/hw03_hard.py
+++ урок 3/hw03_hard.py
@@ -17,7 +17,37 @@
 # то их ЗП уменьшается пропорционально, а за заждый час переработки
 # они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
-
+norm_hors = 185
+f = open('data_hours_of.txt', 'r', encoding='UTF-8')
+nam = f.read()
+nam_all = nam.split()
+l_cont = []
+n = 1
+for ind, i in enumerate(nam_all):
+    if ind == n:
+        l_cont.append(i)
+        n += 2
+l_cont = list(map(int, l_cont))
+norm_mon = 10000
+new_mon = []
+for i in l_cont:
+    coff = round(i / 185, 2)
+    new_mon.append(coff * norm_mon)
+    new_mon = list(map(str, new_mon))
+c = 0
+n = 1
+for ind, i in enumerate(nam_all):
+    for j in new_mon:
+        if ind == n:
+            nam_all.remove(i)
+            nam_all.insert(ind, new_mon[c])
+            n += 2
+            c += 1
+nam_all = ', '.join(nam_all)
+with open('workers.txt', 'w', encoding='UTF-8') as d:
+    d.write(nam_all + '\n')
+f.close
+     
 
 # Задание-3:
 # Дан файл ("data/fruits") со списком фруктов.
@@ -31,3 +61,18 @@
 # Подсказка:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+#list_all = list(map(chr, range(ord('А'), ord('Я') +1)))
+#f = open('data_fruits.txt', 'r', encoding='UTF-8')
+#nam = f.read()
+#nam_all = nam.split()
+#print(nam_all)
+#for i in nam_all:
+#    for j in list_all:
+#        list_j = []
+#        if i[0] == j:
+#            list_j.append(i)
+#       # list_i[0] = []
+#    print(list_j)
+#f.close
+     
